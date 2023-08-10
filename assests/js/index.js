01a4133baa65d176cdd33cec561882a41570c1d0
PIGGYisNOOB <piggyisnoob2007@gmail.com>
2023-08-10 11:59:47 +0700

    gg

---
+++ assests/js/index.js
@@ -0,0 +1,31 @@
+(function() {
+    const processList = document.querySelector('.processList')
+    const performList = document.querySelector('.performList')
+    const leftProfileContainer = document.querySelector('.container-left')
+    const rightProfileContainer = document.querySelector('.container-right')
+    
+    const performtems = processList.querySelectorAll('.content__process-percentage--real')
+    const performItems = performList.querySelectorAll('.content__process-percentage--real')
+    const leftItems = leftProfileContainer.querySelectorAll('.container__section-box')
+    const rightItems = rightProfileContainer.querySelectorAll('.container__section-box')
+
+    function addObserver(items) {
+        for (const item of items) {
+            const observer = new IntersectionObserver(
+                (entries, observer) => entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        setTimeout(() => {
+                            entry.target.classList.add('active-IO')
+                            observer.unobserve(entry.target)
+                        }, 300)
+                    }
+                })
+            )
+    
+            observer.observe(item)
+        }
+    }
+
+    const observerItems = [performtems, performItems, leftItems, rightItems]
+    observerItems.forEach(items => addObserver(items))
+})()
