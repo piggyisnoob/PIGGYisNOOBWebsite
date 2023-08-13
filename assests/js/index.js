e1ed98d4e64a63e7d785fee7619f78b2de525af6
PIGGYisNOOB <piggyisnoob2007@gmail.com>
2023-08-13 23:19:28 +0700

    gg

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,7 +1,29 @@
-const boxs = document.querySelectorAll(".box")
+(function() {
+    const processList = document.querySelector('.processList')
+    const leftProfileContainer = document.querySelector('.container-left')
+    const rightProfileContainer = document.querySelector('.container-right')
+    
+    const performtems = processList.querySelectorAll('.content__process-percentage--real')
+    const leftItems = leftProfileContainer.querySelectorAll('.container__section-box')
+    const rightItems = rightProfileContainer.querySelectorAll('.container__section-box')
 
-const observer = new IntersectionObserver(entries => {
-    console.log(entries)
-})
+    function addObserver(items) {
+        for (const item of items) {
+            const observer = new IntersectionObserver(
+                (entries, observer) => entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        setTimeout(() => {
+                            entry.target.classList.add('active-IO')
+                            observer.unobserve(entry.target)
+                        }, 300)
+                    }
+                })
+            )
+    
+            observer.observe(item)
+        }
+    }
 
-observer.observe(boxs[0])
+    const observerItems = [performtems, performItems, leftItems, rightItems]
+    observerItems.forEach(items => addObserver(items))
+})()
