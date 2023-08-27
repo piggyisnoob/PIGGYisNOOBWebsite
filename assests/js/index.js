6c73086b3a425984a5eaabc8c734f65e59381de9
PIGGYisNOOB <piggyisnoob2007@gmail.com>
2023-08-27 18:08:55 +0700

    gg

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,29 +1,43 @@
-(function() {
-    const processList = document.querySelector('.processList')
-    const leftProfileContainer = document.querySelector('.container-left')
-    const rightProfileContainer = document.querySelector('.container-right')
-    
-    const performtems = processList.querySelectorAll('.content__process-percentage--real')
-    const leftItems = leftProfileContainer.querySelectorAll('.container__section-box')
-    const rightItems = rightProfileContainer.querySelectorAll('.container__section-box')
+// const processList = document.getElementsByClassName("processList");
+// const leftProfileContainer = document.getElementsByClassName("container-left");
+// const rightProfileContainer =
+//   document.getElementsByClassName("container-right");
 
-    function addObserver(items) {
-        for (const item of items) {
-            const observer = new IntersectionObserver(
-                (entries, observer) => entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        setTimeout(() => {
-                            entry.target.classList.add('active-IO')
-                            observer.unobserve(entry.target)
-                        }, 300)
-                    }
-                })
-            )
-    
-            observer.observe(item)
-        }
-    }
+// const performtems = processList.querySelectorAll(
+//   ".content__process-percentage--real"
+// );
+// const leftItems = leftProfileContainer.querySelectorAll(
+//   ".container__section-box"
+// );
+// const rightItems = rightProfileContainer.querySelectorAll(
+//   ".container__section-box"
+// );
 
-    const observerItems = [performtems, performItems, leftItems, rightItems]
-    observerItems.forEach(items => addObserver(items))
-})()
+// function addObserver(items) {
+//   for (const item of items) {
+//     const observer = new IntersectionObserver((entries, observer) =>
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           setTimeout(() => {
+//             entry.target.classList.add("active-IO");
+//             observer.unobserve(entry.target);
+//           }, 300);
+//         }
+//       })
+//     );
+
+//     observer.observe(item);
+//   }
+// }
+
+// const observerItems = [performtems, leftItems, rightItems];
+// observerItems.forEach((items) => addObserver(items));
+
+const loadpage = document.getElementById("loadpage");
+
+const loadpager = document.getElementById("loadpage_r");
+
+window.addEventListener("load", () => {
+  loadpage.style.display = "none";
+  loadpager.style.display = "none";
+});
